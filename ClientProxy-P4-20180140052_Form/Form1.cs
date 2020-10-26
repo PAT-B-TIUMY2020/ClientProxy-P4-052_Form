1cd213af2b09c11953f290cd770957b135deaf3a
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-10-26 21:47:58 +0700

    First Commit

--- ClientProxy-P4-20180140052_Form/Form1.cs
+++ ClientProxy-P4-20180140052_Form/Form1.cs
@@ -16,5 +16,44 @@ namespace ClientProxy_P4_20180140052_Form
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int hasil;
+            hasil = 1 + 2;
+            label2.Text = hasil.ToString();
+            hasil = 3 - 2;
+            label4.Text = hasil.ToString();
+            hasil = 2 * 2;
+            label6.Text = hasil.ToString();
+            hasil = 2 / 2;
+            label8.Text = hasil.ToString();
+
+            double aX = 7;
+            double aY = 8;
+
+            double bX = 5;
+            double bY = 6;
+            double selisihX = aX - bX;
+            double selisihY = aY - bY;
+            double jarak = Math.Sqrt(Math.Pow(selisihX, 2) + Math.Pow(selisihY, 2));
+            label10.Text = jarak.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
